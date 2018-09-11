0d19cfb0fc67d6af6bcc9191b1bd284ee35bd5aa
André Luis Santini <andre.santini@db1.com.br>
2018-09-11 17:42:17 -0300

    start project

---
+++ PocRiotMicroService/src/main/java/application/Main.java
@@ -0,0 +1,15 @@
+package application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Main {
+
+
+    public static void main(String[] args) {
+        System.out.print("Ola mundo");
+
+        SpringApplication.run(Main.class, args);
+    }
+}
